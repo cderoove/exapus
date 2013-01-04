603930e11ac1ed467089790fe7ebc90fff3acb38
radkat <ekaterina.pek@gmail.com>
2013-01-04 21:23:47 +0100

    Optimized calculation of distinct API elements

--- ExapusRAP/src/exapus/model/metrics/NumberReferencedDistinctAPIElementsVisitor.java
+++ ExapusRAP/src/exapus/model/metrics/NumberReferencedDistinctAPIElementsVisitor.java
@@ -3,13 +3,6 @@ package exapus.model.metrics;
 import exapus.model.forest.*;
 import exapus.model.visitors.IForestVisitor;
 
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * Naive and very expensive: almost 100 sec for one project (ant).
- * //TODO: optimize
- */
 public class NumberReferencedDistinctAPIElementsVisitor implements IForestVisitor {
     @Override
     public boolean visitInboundFactForest(InboundFactForest forest) {
@@ -23,56 +16,29 @@ public class NumberReferencedDistinctAPIElementsVisitor implements IForestVisito
 
     @Override
     public boolean visitPackageTree(PackageTree packageTree) {
-        Set<String> apiElements = new HashSet<String>();
-        for (Ref ref : packageTree.getAllReferences()) {
-            apiElements.add(ref.getReferencedName().toString());
-        }
-
-        if (packageTree.getMetric() instanceof NumberReferencedDistinctAPIElements) {
-            ((NumberReferencedDistinctAPIElements) packageTree.getMetric()).setValue(apiElements.size());
-        }
-
         return true;
     }
 
     @Override
     public boolean visitPackageLayer(PackageLayer packageLayer) {
-        Set<String> apiElements = new HashSet<String>();
-        for (Ref ref : packageLayer.getAllReferences()) {
-            apiElements.add(ref.getReferencedName().toString());
-        }
-
-        if (packageLayer.getMetric() instanceof NumberReferencedDistinctAPIElements) {
-            ((NumberReferencedDistinctAPIElements) packageLayer.getMetric()).setValue(apiElements.size());
-        }
-
         return true;
     }
 
     @Override
     public boolean visitMember(Member member) {
-        Set<String> apiElements = new HashSet<String>();
-        for (Ref ref : member.getAllReferences()) {
-            apiElements.add(ref.getReferencedName().toString());
-        }
+        return true;
+    }
 
-        if (member.getMetric() instanceof NumberReferencedDistinctAPIElements) {
-            ((NumberReferencedDistinctAPIElements) member.getMetric()).setValue(apiElements.size());
+    @Override
+    public boolean visitOutboundReference(OutboundRef outboundRef) {
+        if (outboundRef.getMetric() instanceof NumberReferencedDistinctAPIElements) {
+            ((NumberReferencedDistinctAPIElements) outboundRef.getMetric()).addName(outboundRef.getReferencedName().toString(), outboundRef);
         }
-
         return true;
     }
 
     @Override
     public boolean visitInboundReference(InboundRef inboundRef) {
         return false;
-    }
-
-    @Override
-    public boolean visitOutboundReference(OutboundRef outboundRef) {
-        if (outboundRef.getMetric() instanceof NumberReferencedDistinctAPIElements) {
-            ((NumberReferencedDistinctAPIElements) outboundRef.getMetric()).setValue(1);
-        }
-        return true;
     }
 }
