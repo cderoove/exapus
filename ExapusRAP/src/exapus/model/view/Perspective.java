393c31ae0c53dd985323562302e86b69ada29c46
Coen De Roover <cderoove@vub.ac.be>
2013-01-04 14:16:36 +0100

    Possible to edit Project/API selection in View editor, contributes to issue #4.

--- ExapusRAP/src/exapus/model/view/Perspective.java
+++ ExapusRAP/src/exapus/model/view/Perspective.java
@@ -1,5 +1,12 @@
 package exapus.model.view;
 
 public enum Perspective {
-	API_CENTRIC, PROJECT_CENTRIC
+	
+	API_CENTRIC, PROJECT_CENTRIC;
+	
+	private static 	Perspective[] supportedPerspectives = {Perspective.API_CENTRIC, Perspective.PROJECT_CENTRIC};
+	public static Perspective[] supportedPerspectives() {
+		return supportedPerspectives;
+	}
+
 }
