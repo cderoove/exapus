414cd8c495816164a6047685084e4e3e19302419
Coen De Roover <cderoove@vub.ac.be>
2013-01-08 23:10:19 +0100

    Discovered getEnumConstants.

--- ExapusRAP/src/exapus/model/view/Perspective.java
+++ ExapusRAP/src/exapus/model/view/Perspective.java
@@ -4,9 +4,8 @@ public enum Perspective {
 	
 	API_CENTRIC, PROJECT_CENTRIC;
 	
-	private static 	Perspective[] supportedPerspectives = {Perspective.API_CENTRIC, Perspective.PROJECT_CENTRIC};
 	public static Perspective[] supportedPerspectives() {
-		return supportedPerspectives;
+		return Perspective.class.getEnumConstants();
 	}
 
 }
--- ExapusRAP/src/exapus/model/view/Scope.java
+++ ExapusRAP/src/exapus/model/view/Scope.java
@@ -3,11 +3,9 @@ package exapus.model.view;
 public enum Scope {
 
 	ROOT_SCOPE, PREFIX_SCOPE, PACKAGE_SCOPE, TYPE_SCOPE, METHOD_SCOPE;
-	
-	
-	private static Scope[] supportedSelectionScopes = {ROOT_SCOPE, PREFIX_SCOPE, PACKAGE_SCOPE, TYPE_SCOPE, METHOD_SCOPE};
+		
 	public static Scope[] supportedSelectionScopes() {
-		return supportedSelectionScopes;
+		return Scope.class.getEnumConstants();
 	}
 
 	
