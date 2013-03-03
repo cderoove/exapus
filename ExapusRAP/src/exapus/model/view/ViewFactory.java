9ef7821d2c22c59970ad98d659918398726b3fed
Coen De Roover <cderoove@vub.ac.be>
2013-03-03 23:58:59 +0100

    Updated API tag selection test/example.

--- ExapusRAP/src/exapus/model/view/ViewFactory.java
+++ ExapusRAP/src/exapus/model/view/ViewFactory.java
@@ -94,8 +94,8 @@ public class ViewFactory {
 	public View testAPITagSelectionView() {
 		View view = new View("API tag selection test", Perspective.API_CENTRIC);
 		view.addProjectSelection(UniversalSelection.getCurrent());
-		view.addAPISelection(new ScopedSelection(new QName("ant"), Scope.ROOT_SCOPE));
-		view.addAPISelection(new ScopedSelection(new QName("annotation"), Scope.ROOT_SCOPE, "annotation"));
+		view.addAPISelection(new ScopedSelection(new QName("ant"), Scope.TAG_SCOPE));
+		view.addAPISelection(new ScopedSelection(new QName("annotation"), Scope.TAG_SCOPE, "additional"));
 		view.setSourceViewName(TAGGED_API_VIEW_NAME);
 		return view;
 	}
